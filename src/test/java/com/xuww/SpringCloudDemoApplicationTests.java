08f41428a93defa432bcf2b3d253213fa96cca1f
xuww <xu97_16@163.com>
2021-04-07 15:25:39 +0800

    init

---
+++ src/test/java/com/xuww/SpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
